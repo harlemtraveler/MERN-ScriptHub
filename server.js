79a39ada883a3f076c62d6d0471c3e8c4a61e71c
Timothy <timothy.keaveny@troyweb.com>
2018-12-14 17:57:23 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('[+] MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send(
+  'Hello, World!'
+));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(
+  `[*] Server running on port: ${PORT}`
+));
