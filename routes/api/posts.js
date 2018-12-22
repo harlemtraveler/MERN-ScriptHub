fb606304f55ce594e06300562fc77067499b2141
Timothy <timothy.keaveny@troyweb.com>
2018-12-22 15:53:37 -0500

    Add Like functionality for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -102,6 +102,33 @@ router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req,
     });
 });
 
+// @routes POST api/posts/unlike/:id
+// @desc   Unlike post
+// @access Private
+router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Profile.findById(req.params.id)
+        .then(post => {
+          if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ notliked: '[!] You have not yet liked this post' });
+          }
+
+          // Get remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // Splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ nopostfound: '[!] No post found' }));
+    });
+});
+
 module.exports = router;
 
 /*
